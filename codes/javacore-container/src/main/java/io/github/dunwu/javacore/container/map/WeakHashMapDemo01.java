4fde6a6a850a45aea68e610b36e11d88588f6248
lty <843328437@qq.com>
2020-09-22 15:11:58 +0800

    .

--- codes/javacore-container/src/main/java/io/github/dunwu/javacore/container/map/WeakHashMapDemo01.java
+++ codes/javacore-container/src/main/java/io/github/dunwu/javacore/container/map/WeakHashMapDemo01.java
@@ -1,19 +1,23 @@
 package io.github.dunwu.javacore.container.map;
 
+import io.github.dunwu.javacore.container.bean.Person;
+
 import java.util.Map;
 import java.util.WeakHashMap;
 
 public class WeakHashMapDemo01 {
 
     public static void main(String[] args) {
-        Map<String, String> map = null; // 声明Map对象，其中key和value的类型为String
-        map = new WeakHashMap<String, String>();
-        map.put("mldn", "www.mldn.cn");
-        map.put("zhinangtuan", "www.zhinangtuan.net.cn");
-        map.put("mldnjava", "www.mldnjava.cn");
+        Map<String, Person> map = null; // 声明Map对象，其中key和value的类型为String
+        map = new WeakHashMap<>();
+        for (int i = 0; i < 20; i++) {
+            map.put("key"+i, new Person("name" + i, i));
+        }
+        System.out.println("进行垃圾回收之前的map为:"+map);
         System.gc();    // 强制性进行垃圾的收集操作
-        map.put("lxh", "lixinghua");
-        System.out.println(map);
+        map.put("InsertNewKey", new Person("TestName",100));
+        System.out.println("");
+        System.out.println("进行垃圾回收之后的map"+map);
     }
 
 }
